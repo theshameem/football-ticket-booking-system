fbf0d77cbda7a6308570ec2ae31dee9a7718219d
theshameem75 <shameem.alam@selisegroup.com>
2026-06-21 08:49:48 +0600

    feat: booking table creation

--- QUERY.sql
+++ QUERY.sql
@@ -41,20 +41,18 @@ CREATE TABLE Matches (
 -- =========================================================================
 -- 3. CREATE BOOKINGS TABLE
 -- =========================================================================
-CREATE TABLE Bookings (
-    booking_id TYPE,
-    user_id TYPE,
-    match_id TYPE,
-    seat_number TYPE,
-    payment_status TYPE,
-    total_cost TYPE,
-    
-    -- Write your constraint to make 'booking_id' the Primary Key
-    -- Write your Foreign Key constraint linking 'user_id' to the Users table
-    -- Write your Foreign Key constraint linking 'match_id' to the Matches table
-    -- Write your check constraint to ensure 'total_cost' is non-negative
-    -- Write your check constraint to restrict 'payment_status' values
-);
+CREATE TABLE Bookings ( 
+    booking_id INT, 
+    user_id INT, 
+    match_id INT, 
+    seat_number VARCHAR(10), 
+    payment_status VARCHAR(50), 
+    total_cost DECIMAL(10,2), 
+    PRIMARY KEY (booking_id),  -- Write your constraint to make 'booking_id' the Primary Key
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),  -- Write your Foreign Key constraint linking 'user_id' to the Users table
+    FOREIGN KEY (match_id) REFERENCES Matches(match_id), -- Write your Foreign Key constraint linking 'match_id' to the Matches table
+    CHECK (total_cost >= 0), -- Write your check constraint to ensure 'total_cost' is non-negative
+    CHECK ( payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded') OR payment_status IS NULL ) ); -- Write your check constraint to restrict 'payment_status' values
 
 
 -- =========================================================================
