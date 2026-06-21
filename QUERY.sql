7993844207761b945fcb64529593d21d364307ae
theshameem75 <shameem.alam@selisegroup.com>
2026-06-21 09:17:05 +0600

    feat: query four implementation with join

--- QUERY.sql
+++ QUERY.sql
@@ -114,6 +114,14 @@ FROM Bookings
 WHERE payment_status IS NULL;
 
 -- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+SELECT 
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
 
 -- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
 
