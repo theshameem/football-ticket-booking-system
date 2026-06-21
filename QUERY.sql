7cea156a6dc15c7170c41fb29e8dfad41300e0b7
theshameem75 <shameem.alam@selisegroup.com>
2026-06-21 09:20:55 +0600

    feat: query six implementation

--- QUERY.sql
+++ QUERY.sql
@@ -132,5 +132,11 @@ FROM Users u
 LEFT JOIN Bookings b ON u.user_id = b.user_id;
 
 -- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+SELECT 
+    booking_id,
+    match_id,
+    total_cost
+FROM Bookings
+WHERE total_cost > (SELECT AVG(total_cost) FROM Bookings);
 
 -- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
